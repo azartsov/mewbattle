a88c4261dda7dcceac07f77e3124c07020b339d3
Valery Azartsov <valeryazartsov@users.noreply.github.com>
2026-03-19 02:04:22 +0300

    Sync version metadata

--- lib/version.ts
+++ lib/version.ts
@@ -1,11 +1,11 @@
 // Application version
 // Format: 0.(VERSION_OFFSET + git commit count)
-const fallbackAppVersion = "0.226"
-const fallbackGitCommitCount = 36
-const fallbackGitHash = "684de23b"
-const fallbackBuildGeneratedAt = "2026-03-18T22:55:31.004Z"
+const fallbackAppVersion = "0.228"
+const fallbackGitCommitCount = 38
+const fallbackGitHash = "580c032b"
+const fallbackBuildGeneratedAt = "2026-03-18T23:04:16.188Z"
 
 export const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION ?? fallbackAppVersion
-export const APP_GIT_COMMIT_COUNT = Number.parseInt(process.env.NEXT_PUBLIC_APP_GIT_COMMIT_COUNT ?? "36", 10)
+export const APP_GIT_COMMIT_COUNT = Number.parseInt(process.env.NEXT_PUBLIC_APP_GIT_COMMIT_COUNT ?? "38", 10)
 export const APP_GIT_HASH = process.env.NEXT_PUBLIC_APP_GIT_HASH ?? fallbackGitHash
 export const APP_BUILD_GENERATED_AT = process.env.NEXT_PUBLIC_APP_BUILD_GENERATED_AT ?? fallbackBuildGeneratedAt
